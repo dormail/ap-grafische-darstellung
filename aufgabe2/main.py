c767361a1f060a6def535c4feae138aac77ef67a
Matthias Maile <matthias.maile@udo.edu>
2020-12-16 17:53:05 +0100

    schonmal etwas

---
+++ aufgabe2/main.py
@@ -0,0 +1,41 @@
+### main.py ###
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+df = pd.read_csv('daten.csv')
+g = df['g']
+b = df['b']
+G = 1/g
+B = 1/b
+
+# Aufgabe a)
+# Berechnung der Werte fuer f
+f = 1 / (G + B)
+mean = np.mean(f)
+s = np.std(f)
+print(f)
+print(f'Mittelwert: {mean}')
+print(f'Standartabweichung: {s}')
+
+# Aufgabe b)
+# berehchne gerade mit 1/f = m*x+b
+
+m, y0, r, p, std_err  = stats.linregress(G,B)
+x = np.linspace(0,0.02,1000)
+plt.plot(x, m*x + y0, label="Ausgleichsgerade");
+
+plt.scatter(G,B, marker="+", color='k', label="Daten")
+plt.legend()
+
+plt.xlabel(r'$G = 1/g$')
+plt.ylabel(r'$B = 1/b$')
+
+# F = 1/f = G + B = m*x + y0
+# in der lin regress ist G = m*x, also fuer x = 0
+# gilt F = y0 <=> 1/f=y0 <=> f = 1/y0
+print('Laut linregress:')
+print(f'f = {1 / y0}')
+
+plt.show()
