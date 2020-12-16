1648e387c490bced133930a0635498a25a31c32a
Matthias Maile <matthias.maile@udo.edu>
2020-12-16 18:28:00 +0100

    teile aufgabe 1

---
+++ aufgabe1/main.py
@@ -0,0 +1,25 @@
+### main.py ###
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+df = pd.read_csv('daten.csv')
+m = df['m']
+x = df['x']
+
+a, b, r, p, std_err  = stats.linregress(m,x)
+# a = g/k <=> k = g/a
+g = 9.81
+k = g/a
+# bringen k auf si
+k = 0.1*k
+
+m_plt = np.linspace(0,6.5,100)
+plt.plot(m_plt, a*m_plt + b, label=rf'Ausgleichsgerade zu $k = {{{k:.4}}} kg/s^2$');
+plt.scatter(m,x, marker="+", color='k', label="Daten")
+plt.legend()
+plt.xlabel(r'$m/$g')
+plt.ylabel(r'$x/$cm')
+
+plt.show()
